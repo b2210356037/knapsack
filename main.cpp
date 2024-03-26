73dd7c9de1d084d8f774dd1eba8ee9c63ee65817
toprak gungor <toprakgungor02@gmail.com>
2024-03-26 11:46:03 +0300

    knapsack

--- main.cpp
+++ main.cpp
@@ -1,56 +1,36 @@
 #include <iostream>
 #include <vector>
-#include <map>
-#include <queue>
+#include <set>
 #include <algorithm>
+#include <queue>
 #include <map>
 #include <unordered_map>
+#include <cmath>
 using namespace std;
 
-int partition(vector<int>& array, int low, int high){
-    int pivot = array[low];
-    int i = low;
-    int j = high;
+int knapsack(vector<int> weights, vector<int> values, int W) {
+    const int n = weights.size(); //rows
+    vector<vector<int>> dp(n+1, vector<int>(W+1, 0));
 
-    while(i < j){
-        while(array[i] <= pivot){
-            i++;
-        }
-        while(array[j] > pivot){
-            j--;
-        }
-        if(i < j){
-            swap(array[i], array[j]);
+    for(int i = 1; i <= n; i++){
+        for(int j = 0; j < W+1; j++){ //j represnts all the possible weight options
+            if(j - weights[i-1] >= 0){
+                dp[i][j] = max(values[i-1] + dp[i-1][j-weights[i-1]], dp[i-1][j]);
+            }
+            else{
+                dp[i][j] = dp[i-1][j];
+            }
         }
     }
-    swap(array[j], array[low]);
-    return j;
-}
-
-void quicksort(vector<int>& array, int low, int high){
-    if(low < high){
-        int j = partition(array, low, high);
-        quicksort(array, low, j);
-        quicksort(array, j+1, high);
-    }
-
+    return dp[n-1][W];
+    
 }
 
 int main() {
-    std::vector<int> array = {12, 4, 5, 6, 7, 3, 1, 15};
-
-    std::cout << "Original array: ";
-    for (int num : array) {
-        std::cout << num << " ";
-    }
-
-    int arraySize = array.size();
-    quicksort(array, 0, arraySize - 1);
-
-    std::cout << "\nSorted array: ";
-    for (int num : array) {
-        std::cout << num << " ";
-    }
-
+    int total = 7;
+    vector<int> weights = {1, 3, 4, 5};
+    vector<int> values = {1, 4, 5, 7}; 
+	int res = knapsack(weights, values, total);
+    cout << res;
     return 0;
 }
